dd0b5278abe371b64d06abc883217c6814a5565c
Bartek Rachwal <brachwal@agh.edu.pl>
2023-02-21 22:25:22 +0100

    extract colors to separate file

--- library/include/ConfigSvc.hh
+++ library/include/ConfigSvc.hh
@@ -2,27 +2,6 @@
 // Created by brachwal on 23.03.2020.
 //
 
-#ifndef _COLORS_
-#define _COLORS_
-// Usage example
-// std::cout << FGRN("I'm blue.") << std::endl;
-#define RST  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
-#define FRED(x) KRED x RST
-#define FGRN(x) KGRN x RST
-#define FYEL(x) KYEL x RST
-#define FBLU(x) KBLU x RST
-#define FMAG(x) KMAG x RST
-#define FCYN(x) KCYN x RST
-#define FWHT(x) KWHT x RST
-#endif  /* _COLORS_ */
-
 #ifndef CONFIGSVC_CONFIGSVC_HH
 #define CONFIGSVC_CONFIGSVC_HH
 
